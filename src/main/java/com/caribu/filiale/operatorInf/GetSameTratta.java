68c0e382257c058fb82e61f4034dc38289655143
NikiDiCostanzo <niki.dicostanzo@stud.unifi.it>
2023-06-28 14:16:48 +0200

    GET postgis.

--- src/main/java/com/caribu/filiale/operatorInf/GetSameTratta.java
+++ src/main/java/com/caribu/filiale/operatorInf/GetSameTratta.java
@@ -46,36 +46,30 @@ public class GetSameTratta implements Handler<RoutingContext> {
 //         }
 //    
 }
-
-
     private void getTratta(RoutingContext context){
             // FIRENZE -> ROMA
-        // oLat="43,7792500"&oLon="11.2462600"&dLat="41.8919300"&dLon="2.5113300"
-        
-
-        // String longitudine = context.request().getParam("longitudine");
-        // String latitudine = context.request().getParam("latitudine");
-        // String id_tratta = context.request().getParam("id_tratta");
+        // oLat=43,7792500&oLon=11.2462600&dLat=41.8919300&dLon=12.5113300
         Float oLat = Float.parseFloat(context.request().getParam("oLat"));
         Float oLon =  Float.parseFloat(context.request().getParam("oLon"));
         Float dLat =  Float.parseFloat(context.request().getParam("dLat"));
         Float dLon =  Float.parseFloat(context.request().getParam("dLon"));
 
         // Create origin and destination geometry points
-
         Map<String, Object> parameters = new HashMap<>();
                     parameters.put("oLat", oLat);
                     parameters.put("oLon", oLon);
                     parameters.put("dLat", dLat);
                     parameters.put("dLon", dLon);
 
-                    //BETWEEN (#{input_distance} - 10) AND (#{input_distance} + 10)
+         //BETWEEN (#{input_distance} - 10) AND (#{input_distance} + 10)
          // AND ST_DistanceSphere(#{destPoint}, #{originPoint}) < 10000")
          String input_Ogeo= "ST_SetSRID(ST_MakePoint(#{oLon} , #{oLat}), 4326)";
          String input_Dgeo= "ST_SetSRID(ST_MakePoint(#{dLon} , #{dLat}), 4326)";
-         String distance = "ST_DistanceSphere("+input_Ogeo+ "," + input_Dgeo + ")"; 
-         String query= "SELECT * from schema.tratta o where ST_DistanceSphere(o.origin_geom, o.destination_geom) <=" + distance + " - 100 AND ST_DistanceSphere(o.origin_geom, o.destination_geom) > "+ distance + " + 100";
-        SqlTemplate.forQuery(db,query)
+         String distance = "ST_DistanceSphere("+ input_Ogeo + "," + input_Dgeo + ")"; 
+         
+         String query= "SELECT *, ST_DistanceSphere(o.origin_geom, o.destination_geom) as dist from schema.tratta o where ST_DistanceSphere(o.origin_geom, o.destination_geom) BETWEEN "+distance+"-100 AND "+distance+"+100";
+         
+         SqlTemplate.forQuery(db,query)
             .rxExecute(parameters)  //TODO
             .doOnError(err -> {
                 LOG.debug("Failure: ", err , err.getMessage());
@@ -90,6 +84,7 @@ public class GetSameTratta implements Handler<RoutingContext> {
                 JsonArray response = new JsonArray();
                 result.forEach(row -> {
                     JsonObject rowJson = new JsonObject()
+                    .put("dist", row.getValue("dist"))    
                     .put("id_tratta", row.getValue("id_tratta"))
                     .put("latitudine", row.getValue("latitudine"))
                     .put("longitudine", row.getValue("longitudine"))
@@ -132,8 +127,58 @@ public class GetSameTratta implements Handler<RoutingContext> {
             })
             .subscribe(sr->{},err->{LOG.debug("FailureSbu: ", err , err.getMessage());
                 }); // Don't forget to subscribe to the Single
-            }
+    }
+    
+    private void getDistance(RoutingContext context){
+            // FIRENZE -> ROMA
+        // oLat="43,7792500"&oLon="11.2462600"&dLat="41.8919300"&dLon="2.5113300"
+        Float oLat = Float.parseFloat(context.request().getParam("oLat"));
+        Float oLon =  Float.parseFloat(context.request().getParam("oLon"));
+        Float dLat =  Float.parseFloat(context.request().getParam("dLat"));
+        Float dLon =  Float.parseFloat(context.request().getParam("dLon"));
+
+        // Create origin and destination geometry points
+        Map<String, Object> parameters = new HashMap<>();
+                    parameters.put("oLat", oLat);
+                    parameters.put("oLon", oLon);
+                    parameters.put("dLat", dLat);
+                    parameters.put("dLon", dLon);
+
+                    //BETWEEN (#{input_distance} - 10) AND (#{input_distance} + 10)
+         // AND ST_DistanceSphere(#{destPoint}, #{originPoint}) < 10000")
+         String input_Ogeo= "ST_SetSRID(ST_MakePoint(#{oLon} , #{oLat}), 4326)";
+         String input_Dgeo= "ST_SetSRID(ST_MakePoint(#{dLon} , #{dLat}), 4326)";
+         String distance = "ST_DistanceSphere("+input_Ogeo+ "," + input_Dgeo + ")"; 
+         String query= "SELECT " + distance +" as dist";
+        SqlTemplate.forQuery(db,query)
+            .rxExecute(parameters)  //TODO
+            .doOnError(err -> {
+                LOG.debug("Failure: ", err , err.getMessage());
+                context.response()
+                    .putHeader(HttpHeaders.CONTENT_TYPE, HttpHeaderValues.APPLICATION_JSON)
+                    .setStatusCode(500)
+                    .end(new JsonObject().put("error", err.getMessage()).encode());
+            })
+            .doOnSuccess(result -> {
+                //TODO errore nome sbagliato
+                LOG.info("Got " + result.size() + " rows ");
+                JsonArray response = new JsonArray();
+                result.forEach(row -> {
+                    JsonObject rowJson = new JsonObject()
+                    .put("dist", row.getValue("dist"));    
+                    response.add(rowJson);
+                });
+                    LOG.info("Path {} responds with {}", context.normalizedPath(), response.encode());
+                    context.response()
+                    .putHeader(HttpHeaders.CONTENT_TYPE, HttpHeaderValues.APPLICATION_JSON)
+                    .end(response.encode());
+                }).subscribe(succ->{},err -> {
+                LOG.debug("Failure2: ", err , err.getMessage());});
+        }
+
 }
+
+
 
     
 
